7b6fa3bb19241c45c982ec1dd566fc4c74c5a481
shine7658 <shine941011@gmail.com>
2025-11-17 14:37:27 +0800

    v6.畫出一匹賽馬

--- app/src/main/java/tw/edu/pu/csim/s1131213/race/GameScreen.kt
+++ app/src/main/java/tw/edu/pu/csim/s1131213/race/GameScreen.kt
@@ -14,13 +14,20 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.window.embedding.EmbeddingBounds
 
 @Composable
 fun GameScreen(message: String,gameViewModel: GameViewModel) {
+
+    val imageBitmap = ImageBitmap.imageResource(R.drawable.horse0)
+
 
     Box(modifier = Modifier
         .fillMaxSize()
@@ -49,6 +56,12 @@ fun GameScreen(message: String,gameViewModel: GameViewModel) {
                 color = Color.Red,
                 radius = 100f,
                 center = Offset(gameViewModel.circleX,gameViewModel.circleY)
+            )
+
+            drawImage(
+                image = imageBitmap,
+                dstOffset = IntOffset(0,100),
+                dstSize = IntSize(200,200)
             )
         }
 
